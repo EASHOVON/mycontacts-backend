d681482f7c4ca2bbbf6d03c3eaa5b7ea5eb1bf42
EASHOVON” <ashrafuzzaman.shovon.786@gmail.com>
2023-04-06 12:05:39 +0600

    Initial Operation and testing api done

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("I am now the express project!")
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/api/mycontacts", (req, res) =>
+{
+    res.json({ message: "Get All Contacts!" });
+})
+
+app.listen(port, () =>
+{
+    console.log(`Server running on ${ port }`);
+})
